af5623f3ed507c18600548d973433be60a062a52
Alexandre Nicol <nicol.alexandre1@gmail.com>
2021-05-26 08:19:48 +0000

    radio nova skill and lambda

--- _lambda/src/handler.ts
+++ _lambda/src/handler.ts
@@ -3,19 +3,98 @@ import {
   RequestHandler,
   SkillBuilders,
   getRequestType,
+  getIntentName,
 } from 'ask-sdk-core';
 import { Response } from 'ask-sdk-model';
+import { CommonHandlerFactory } from './request-handlers/common';
+
+const STREAM_URL = 'https://novazz.ice.infomaniak.ch/novazz-128.mp3';
 
 const LaunchRequestHandler: RequestHandler = {
   canHandle(handlerInput: HandlerInput): boolean {
     return getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
   },
   handle(handlerInput: HandlerInput): Response {
-    const speakOutput = 'hi there';
-    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+    return handlerInput.responseBuilder
+      .addAudioPlayerPlayDirective('REPLACE_ALL', STREAM_URL, STREAM_URL, 0)
+      .withShouldEndSession(true)
+      .getResponse();
   },
 };
 
+const PlayIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      getIntentName(handlerInput.requestEnvelope) === 'Play'
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    return handlerInput.responseBuilder
+      .addAudioPlayerPlayDirective('REPLACE_ALL', STREAM_URL, STREAM_URL, 0)
+      .withShouldEndSession(true)
+      .getResponse();
+  },
+};
+
+const PauseIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.PauseIntent'
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    return handlerInput.responseBuilder
+      .addAudioPlayerStopDirective()
+      .getResponse();
+  },
+};
+
+const ResumeIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      getIntentName(handlerInput.requestEnvelope) === 'AMAZON.ResumeIntent'
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    return handlerInput.responseBuilder
+      .addAudioPlayerPlayDirective('REPLACE_ALL', STREAM_URL, STREAM_URL, 0)
+      .withShouldEndSession(true)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      (getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent' ||
+        getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent')
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    return handlerInput.responseBuilder
+      .addAudioPlayerStopDirective()
+      .getResponse();
+  },
+};
+
+const commonHandlers = CommonHandlerFactory('say, play');
+
 exports.handler = SkillBuilders.custom()
-  .addRequestHandlers(LaunchRequestHandler)
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    PlayIntentHandler,
+    PauseIntentHandler,
+    ResumeIntentHandler,
+    CancelAndStopIntentHandler,
+    commonHandlers.HelpIntentHandler,
+    commonHandlers.SessionEndedRequestHandler,
+    commonHandlers.IntentReflectorHandler
+  )
+  .addErrorHandlers(
+    commonHandlers.CustomErrorHandler
+  )
   .lambda();
